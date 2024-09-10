b61987cf26183b01b967a625c97d9fce03c2183f
Tiago Teague Violante <339784910@GoTVDSB.ca>
2024-09-10 09:30:23 -0400

    Add project files.

---
+++ Topic-2  Numbers and Math Assignment/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Topic_2__Numbers_and_Math_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tiago
+            Console.WriteLine();
+            Console.WriteLine("I was born" + (2024 - 16));
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("The average of 14, 15, and 16 is");
+            Console.WriteLine((14.0 + 15.0 + 16.0) / 3 );
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("The average of 14, 15, 16, and 17 is");
+            Console.WriteLine((14.0 + 15.0 + 16.0 + 17.0) / 4);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("The area of circle with a radius a 3cm is");
+            Console.WriteLine((3.14 * 9 ));
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("C# knows when to use the + operator instead of sticking them together");
+            Console.WriteLine("when both sides of it are intagers and not strings");
+
+            Console.ReadLine();
+        }
+    }
+}
